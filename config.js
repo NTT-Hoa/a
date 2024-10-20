3d311e576e843ed94427fe2e00eaff500ed77323
NTT-Hoa <tule141227@gmail.com>
2024-10-20 19:05:01 +0700

    Huyền Khương

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thanh Huyền';
+const giftUrl = 'https://zalo.me/0368444367';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng mẹ Thanh Huyền';
+const contentCard = 'Chúc mừng 20-10, mẹ Huyền yêu dấu! 💐
+Nhân ngày đặc biệt này, con chỉ muốn nói rằng con rất biết ơn vì có mẹ trong cuộc đời. Mẹ à, dù thời gian có trôi đi nhưng tình yêu và sự hy sinh của mẹ mãi mãi không bao giờ thay đổi. Chúc mẹ luôn tươi trẻ, khỏe mạnh và hạnh phúc bên gia đình. Ngày 20-10 thật vui vẻ mẹ nhé! ❤️
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'HuyenKhuong.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
